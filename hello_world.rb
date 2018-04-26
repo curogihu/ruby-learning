70eebaba2a49c5ca13df8ef84146b8697f6d6694
hikozuma <curoas@yahoo.co.jp>
2018-04-26 07:37:16 +0000

    first commit

---
+++ hello_world.rb
@@ -0,0 +1,2 @@
+puts "Hello world"
+puts "Welcome to the complete ruby on rails developer course"
---
+++ hello_world_method.rb
@@ -0,0 +1,12 @@
+# method is something that performs an action for us
+
+def say_hello
+   puts "Hello world"
+end
+
+def say_something(anything)
+    puts anything
+end
+
+say_hello
+say_something("Welcome to the complete ruby on rails developer course")
