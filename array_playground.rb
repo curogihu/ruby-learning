ab850b41a82351889ba7a8a5bd85a4ebaa08d8f8
hikozuma <curoas@yahoo.co.jp>
2018-04-27 02:46:48 +0000

    add array method

---
+++ array_playground.rb
@@ -0,0 +1,23 @@
+a = (0..10).to_a
+
+a.each {|i| puts i}
+
+# include
+# shuffle!
+# uniq
+
+=begin
+for number in a
+    puts "hi"
+end
+
+names.each do |food|
+    puts "Hello #{food}"
+    puts "Hello #{food.capitalize}"
+end
+
+y.select { |number| print number if number.odd? }
+y.each { |number| print number if number.odd? }
+
+some_array.join
+=end
