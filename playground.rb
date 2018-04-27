665f2085d17cf8db2acd7390ffc6581d3141e2ac
hikozuma <curoas@yahoo.co.jp>
2018-04-27 02:06:29 +0000

    add four functions

--- playground.rb
+++ playground.rb
@@ -1,8 +1,25 @@
+def multiply(first_number, second_number)
+    first_number.to_f * second_number.to_f
+end
+
+def divide(first_number, second_number)
+    first_number.to_f / second_number.to_f
+end
+
+def subtract(first_number, second_number)
+    first_number.to_f - second_number.to_f
+end
+
+def mod(first_number, second_number)
+    first_number.to_f % second_number.to_f
+end
+
 # puts "hikozuma"  + " "  + "Curogihu"
 # "My name is #{first_name}"
 # 'Hey Chris, John said \'How are you doing\''
 # "aaaaa".length
 
+=begin
 puts "Hello enter in your first name"
 first_name = gets.chomp
 
@@ -14,6 +31,10 @@ puts "Welcome #{first_name} #{last_name}, to the jungle"
 puts "Your name length is #{first_name.length + last_name.length}"
 
 # 20.times {puts "hi"} -> display hi, 20 times
+=end
+
+puts "what do you want to do? 1) multiply 2) divide 3) subtract 4) find remainder"
+prompt = gets.chomp
 
 puts "Please enter your first number"
 first_number = gets.chomp
@@ -21,6 +42,31 @@ first_number = gets.chomp
 puts "Please enter your second number"
 second_number = gets.chomp
 
-puts "First number multiplied by second number is: #{first_number.to_i * second_number.to_i}"
-puts "First number subtracted by second number is: #{first_number.to_i - second_number.to_i}"
-puts "First number mod by second number is: #{first_number.to_i % second_number.to_i}"
+if prompt == '1'
+    puts "You have chosen to multiply #{first_number} with #{second_number}"
+    result = multiply(first_number, second_number)
+    
+elsif prompt == '2'
+    puts "You have chosen to divide"
+    result = divide(first_number, second_number)
+
+elsif prompt == '3'
+    puts "You have chosen to subtract"
+    result = subtract(first_number, second_number)
+
+elsif prompt == '4'
+    puts "You have chosen to find the remainder"    
+    result = mod(first_number, second_number)
+    
+else
+    puts "You have made an invalid choice"
+    
+end
+
+puts "The result is #{result}"
+    
+=begin
+puts "First number multiplied by second number is: #{multiply(first_number, second_number)}"
+puts "First number subtracted by second number is: #{subtract(first_number, second_number)}"
+puts "First number mod by second number is: #{mod(first_number, second_number)}"
+=end
